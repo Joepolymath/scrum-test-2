49b62a900251d049987edf303ee9fa21d5554484
Sadeeq <sadikutaahmad2020@gmail.com>
2021-07-28 01:58:49 +0100

    Delete secondary directory

--- secondary/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import student
-
-class studentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = student
-        #fields = ('firstName', 'lastName', 'email')
-        fields = '__all__'
